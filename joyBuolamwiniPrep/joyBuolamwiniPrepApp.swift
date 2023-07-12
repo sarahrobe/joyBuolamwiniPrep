eb4fd0832c83ee3997804f7c49b03630dd35bbcf
Sarah Roberts <sarahrobe@comcast.net>
2023-07-11 20:08:02 -0500

    Initial Commit

---
+++ joyBuolamwiniPrep/joyBuolamwiniPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  joyBuolamwiniPrepApp.swift
+//  joyBuolamwiniPrep
+//
+//  Created by Sarah Roberts on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct joyBuolamwiniPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
